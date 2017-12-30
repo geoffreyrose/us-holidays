4ffa5d4e4fea16e0faa6eee0af745fd569774d11
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2017-12-30 15:12:53 -0800

    added support for 25 holidays and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# US-Holidays
-Helps Calculate if a day is a US Holidays   
+# Carbon Support for US Holidays
+***
+This extends [Carbon](http://carbon.nesbot.com/) and adds support for several US holidays.
+
+### Supported Holidays
+ * New Years Day
+ * Martin Luther King Jr. Day
+ * Groundhog Day
+ * Valentine's Day
+ * Presidents Day
+ * Daylight Saving (End)
+ * St. Patrick's Day
+ * April Fool's Day
+ * Mother's Day
+ * Armed Forces Day
+ * Memorial Day
+ * Flag Day
+ * Father's Day
+ * Independence Day
+ * Labor Day
+ * Patriot Day
+ * Columbus Day
+ * Halloween
+ * Veterans Day
+ * Thanksgiving
+ * Daylight Saving (End)
+ * Pearl Harbor Remembrance Day
+ * Christmas Eve
+ * Christmas Day
+ * New Years Eve
+
+
+### Usage
+
+Basic Setup
+
+    <?php
+    require 'Carbon.php';
+    require 'USHolidays.php';
+
+    use USHolidays\Carbon;
+
+Check if date is holiday. Returns `boolean`
+
+    $carbon = new Carbon();
+    $carbon = Carbon::create(2018, 12, 25);
+    $carbon->isHoliday(); // bool (true)
+
+Get name if date is holiday. Returns `string` or `false`
+
+    $carbon = new Carbon();
+    $carbon = Carbon::create(2018, 12, 31);
+    $carbon->getHolidayName(); // New Year's Eve
+
+Get date for a specific holiday. Returns `string`
+
+    $carbon = new Carbon();
+    $carbon = Carbon::create(2018, 1, 1);
+
+    $carbon->getNewYearsDayHoliday();                   // 2018-01-01 00:00:00
+    $carbon->getMLKDayHoliday();                        // 2018-01-15 00:00:00
+    $carbon->getGroundhogDayHoliday();                  // 2018-02-02 00:00:00
+    $carbon->getValentinesDayHoliday();                 // 2018-02-14 00:00:00
+    $carbon->getPresidentsDayHoliday();                 // 2018-02-19 00:00:00
+    $carbon->getDaylightSavingStartHoliday();           // 2018-03-11 00:00:00
+    $carbon->getStPatricksDayHoliday();                 // 2018-03-17 00:00:00
+    $carbon->getAprilFoolsDayHoliday();                 // 2018-04-01 00:00:00
+    $carbon->getMothersDayHoliday();                    // 2018-05-13 00:00:00
+    $carbon->getArmedForcesDayHoliday();                // 2018-05-19 00:00:00
+    $carbon->getMemorialDayHoliday();                   // 2018-05-28 00:00:00
+    $carbon->getFlagsDayHoliday();                      // 2018-06-14 00:00:00
+    $carbon->getFathersDayHoliday();                    // 2018-06-17 00:00:00
+    $carbon->getIndependenceDayHoliday();               // 2018-07-04 00:00:00
+    $carbon->getLaborDayHoliday();                      // 2018-09-03 00:00:00
+    $carbon->getPatriotsDayHoliday();                   // 2018-09-11 00:00:00
+    $carbon->getColumbusDayHoliday();                   // 2018-10-08 00:00:00
+    $carbon->getHalloweenDayHoliday();                  // 2018-10-31 00:00:00
+    $carbon->getDaylightSavingEndHoliday();             // 2018-11-04 00:00:00
+    $carbon->getVeteransDayHoliday();                   // 2018-11-11 00:00:00
+    $carbon->getThanksgivingHoliday();                  // 2018-11-22 00:00:00
+    $carbon->getPearlHarborRemembranceHoliday();        // 2018-12-07 00:00:00
+    $carbon->getChristmasEveHoliday();                  // 2018-12-24 00:00:00
+    $carbon->getChristmasDayHoliday();                  // 2018-12-25 00:00:00
+    $carbon->getNewYearsEveHoliday();                   // 2018-12-31 00:00:00
+
+
+### Additional Examples    
+
+    $carbon = new Carbon();
+    $carbon = Carbon::create(2018, 07, 04);
+    $carbon->getIndependenceDayHoliday()->getHolidayName(); // Independence Day
--- USHolidays.php
+++ USHolidays.php
@@ -54,10 +54,45 @@ class Carbon extends \Carbon\Carbon {
         $thanksgiving->next(Carbon::THURSDAY)->next(Carbon::THURSDAY)->next(Carbon::THURSDAY);
 
 
+        // Daylight Saving (Start)
+        $daylightStart = Carbon::create($year, 3, 1);
+        if( $daylightStart->dayOfWeek !== Carbon::SUNDAY ) {
+        	$daylightStart->next(Carbon::SUNDAY);
+        }
+        $daylightStart->next(Carbon::SUNDAY);
+
+        // Mothers Day
+        $mothers = Carbon::create($year, 5, 1);
+        if( $mothers->dayOfWeek !== Carbon::SUNDAY ) {
+        	$mothers->next(Carbon::SUNDAY);
+        }
+        $mothers->next(Carbon::SUNDAY);
+
+        // Armed Forces Day
+        $armed = Carbon::create($year, 5, 1);
+        if( $armed->dayOfWeek !== Carbon::SATURDAY ) {
+            $armed->next(Carbon::SATURDAY);
+        }
+        $armed->next(Carbon::SATURDAY)->next(Carbon::SATURDAY);
+
+        // Father Day
+        $father = Carbon::create($year, 6, 1);
+        if( $father->dayOfWeek !== Carbon::SUNDAY ) {
+        	$father->next(Carbon::SUNDAY);
+        }
+        $father->next(Carbon::SUNDAY)->next(Carbon::SUNDAY);
+
+        // Daylight Saving (End)
+        $daylightEnd = Carbon::create($year, 11, 1);
+        if( $daylightEnd->dayOfWeek !== Carbon::SUNDAY ) {
+        	$daylightEnd->next(Carbon::SUNDAY);
+        }
+
+
         $holidays = array(
         	array(
                 'name' => "New Year's Day",
-                'date' => Carbon::create($year, 01, 01),
+                'date' => Carbon::create($year, 1, 1),
                 'bank_holiday' => true,
                 'id' => 1
             ),
@@ -67,65 +102,143 @@ class Carbon extends \Carbon\Carbon {
                 'bank_holiday' => true,
                 'id' => 2
             ),
-        	array(
-                'name' => "Presidents' Day",
-                'date' => $presidents,
+            array(
+                'name' => "Groundhog Day",
+                'date' => Carbon::create($year, 2, 2),
                 'bank_holiday' => true,
                 'id' => 3
+            ),
+            array(
+                'name' => "Valentine's Day",
+                'date' => Carbon::create($year, 2, 14),
+                'bank_holiday' => true,
+                'id' => 4
+            ),
+        	array(
+                'name' => "President's Day",
+                'date' => $presidents,
+                'bank_holiday' => true,
+                'id' => 5
+            ),
+            array(
+                'name' => "Daylight Saving (Start)",
+                'date' => $daylightStart,
+                'bank_holiday' => true,
+                'id' => 6
+            ),
+            array(
+                'name' => "St. Patrick's Day",
+                'date' => Carbon::create($year, 3, 17),
+                'bank_holiday' => true,
+                'id' => 7
+            ),
+            array(
+                'name' => "April Fool's Day",
+                'date' => Carbon::create($year, 4, 1),
+                'bank_holiday' => true,
+                'id' => 8
+            ),
+            array(
+                'name' => "Mother's Day",
+                'date' => $mothers,
+                'bank_holiday' => true,
+                'id' => 9
             ),
         	array(
                 'name' => "Memorial Day",
                 'date' => $memorial,
                 'bank_holiday' => true,
-                'id' => 4
+                'id' => 10
+            ),
+            array(
+                'name' => "Armed Forces Day",
+                'date' => $armed,
+                'bank_holiday' => true,
+                'id' => 11
+            ),
+            array(
+                'name' => "Father's Day",
+                'date' => $father,
+                'bank_holiday' => true,
+                'id' => 12
+            ),
+            array(
+                'name' => "Flag Day",
+                'date' => Carbon::create($year, 6, 14),
+                'bank_holiday' => true,
+                'id' => 13
             ),
             array(
                 'name' => "Independence Day",
-                'date' => Carbon::create($year, 07, 04),
+                'date' => Carbon::create($year, 7, 4),
                 'bank_holiday' => true,
-                'id' => 5
+                'id' => 14
             ),
             array(
                 'name' => "Labor Day",
                 'date' => $labor,
                 'bank_holiday' => true,
-                'id' => 6
+                'id' => 15
+            ),
+            array(
+                'name' => "Patriot Day",
+                'date' => Carbon::create($year, 9, 11),
+                'bank_holiday' => true,
+                'id' => 16
             ),
         	array(
                 'name' => "Columbus Day",
                 'date' => $columbus,
                 'bank_holiday' => true,
-                'id' => 7
+                'id' => 17
             ),
         	array(
+                'name' => "Halloween",
+                'date' => Carbon::create($year, 10, 31),
+                'bank_holiday' => true,
+                'id' => 18
+            ),
+            array(
+                'name' => "Daylight Saving (End) ",
+                'date' => $daylightEnd,
+                'bank_holiday' => true,
+                'id' => 19
+            ),
+            array(
                 'name' => "Veterans Day",
                 'date' => Carbon::create($year, 11, 11),
                 'bank_holiday' => true,
-                'id' => 8
+                'id' => 20
             ),
             array(
                 'name' => "Thanksgiving",
                 'date' => $thanksgiving,
                 'bank_holiday' => true,
-                'id' => 9
+                'id' => 21
+            ),
+            array(
+                'name' => "Pearl Harbor Remembrance Day",
+                'date' => Carbon::create($year, 12, 7),
+                'bank_holiday' => false,
+                'id' => 22
             ),
             array(
                 'name' => "Christmas Eve",
                 'date' => Carbon::create($year, 12, 24),
                 'bank_holiday' => false,
-                'id' => 10
+                'id' => 23
             ),
         	array(
                 'name' => "Christmas Day",
                 'date' => Carbon::create($year, 12, 25),
                 'bank_holiday' => true,
-                'id' => 11
+                'id' => 24
             ),
             array(
                 'name' => "New Year's Eve",
                 'date' => Carbon::create($year, 12, 31),
                 'bank_holiday' => false,
-                'id' => 12
+                'id' => 25
             ),
         );
 
@@ -212,7 +325,7 @@ class Carbon extends \Carbon\Carbon {
         return $date;
     }
 
-    public function getPresidentsDayHoliday($year = null)
+    public function getGroundhogDayHoliday($year = null)
     {
         $year = $year ? $year : $this->year;
         $holidays = $this->getHolidays($year);
@@ -228,7 +341,7 @@ class Carbon extends \Carbon\Carbon {
         return $date;
     }
 
-    public function getMemorialDayHoliday($year = null)
+    public function getValentinesDayHoliday($year = null)
     {
         $year = $year ? $year : $this->year;
         $holidays = $this->getHolidays($year);
@@ -244,7 +357,7 @@ class Carbon extends \Carbon\Carbon {
         return $date;
     }
 
-    public function getIndependenceDayHoliday($year = null)
+    public function getPresidentsDayHoliday($year = null)
     {
         $year = $year ? $year : $this->year;
         $holidays = $this->getHolidays($year);
@@ -260,7 +373,7 @@ class Carbon extends \Carbon\Carbon {
         return $date;
     }
 
-    public function getLaborDayHoliday($year = null)
+    public function getDaylightSavingStartHoliday($year = null)
     {
         $year = $year ? $year : $this->year;
         $holidays = $this->getHolidays($year);
@@ -276,7 +389,7 @@ class Carbon extends \Carbon\Carbon {
         return $date;
     }
 
-    public function getColumbusDayHoliday($year = null)
+    public function getStPatricksDayHoliday($year = null)
     {
         $year = $year ? $year : $this->year;
         $holidays = $this->getHolidays($year);
@@ -292,9 +405,7 @@ class Carbon extends \Carbon\Carbon {
         return $date;
     }
 
-
-
-    public function getVeteransDayHoliday($year = null)
+    public function getAprilFoolsDayHoliday($year = null)
     {
         $year = $year ? $year : $this->year;
         $holidays = $this->getHolidays($year);
@@ -310,7 +421,7 @@ class Carbon extends \Carbon\Carbon {
         return $date;
     }
 
-    public function getThanksgivingHoliday($year = null)
+    public function getMothersDayHoliday($year = null)
     {
         $year = $year ? $year : $this->year;
         $holidays = $this->getHolidays($year);
@@ -326,7 +437,7 @@ class Carbon extends \Carbon\Carbon {
         return $date;
     }
 
-    public function getChristmasEveHoliday($year = null)
+    public function getMemorialDayHoliday($year = null)
     {
         $year = $year ? $year : $this->year;
         $holidays = $this->getHolidays($year);
@@ -342,7 +453,7 @@ class Carbon extends \Carbon\Carbon {
         return $date;
     }
 
-    public function getChristmasDayHoliday($year = null)
+    public function getArmedForcesDayHoliday($year = null)
     {
         $year = $year ? $year : $this->year;
         $holidays = $this->getHolidays($year);
@@ -358,7 +469,7 @@ class Carbon extends \Carbon\Carbon {
         return $date;
     }
 
-    public function getNewYearsEveHoliday($year = null)
+    public function getFathersDayHoliday($year = null)
     {
         $year = $year ? $year : $this->year;
         $holidays = $this->getHolidays($year);
@@ -366,6 +477,215 @@ class Carbon extends \Carbon\Carbon {
 
         foreach ($holidays as $holiday) {
             if( $holiday['id'] === 12 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getFlagsDayHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 13 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getIndependenceDayHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 14 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+
+    public function getLaborDayHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 15 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getPatriotsDayHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 16 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getColumbusDayHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 17 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getHalloweenDayHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 18 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getDaylightSavingEndHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 19 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getVeteransDayHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 20 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getThanksgivingHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 21 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getPearlHarborRemembranceHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 22 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getChristmasEveHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 23 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getChristmasDayHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 24 ) {
+                $date = $holiday['date'] ;
+            }
+        }
+
+        $this->modify($date);
+        return $date;
+    }
+
+    public function getNewYearsEveHoliday($year = null)
+    {
+        $year = $year ? $year : $this->year;
+        $holidays = $this->getHolidays($year);
+        $date = false;
+
+        foreach ($holidays as $holiday) {
+            if( $holiday['id'] === 25 ) {
                 $date = $holiday['date'] ;
             }
         }
