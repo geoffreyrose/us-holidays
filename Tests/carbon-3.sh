e73654155144bf8dcf5700b4d8a96c3c87d26383
geoffreyrose <geoffrey@geoffreyrose.io>
2024-02-01 16:33:55 -0800

    support carbon v3

---
+++ Tests/carbon-3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp composer.json original-composer.json
+rm composer.lock
+
+rm -rf vendor
+composer remove nesbot/carbon
+composer require nesbot/carbon:3.* --no-plugins
+
+rm composer.json
+mv original-composer.json composer.json
+
+./vendor/bin/phpunit
